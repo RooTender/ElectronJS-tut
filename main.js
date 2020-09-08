c4f4ba9eeda52d736dabc0c427fdccba0d63987d
Hubert <lewandowski.hubert486@gmail.com>
2020-09-08 11:12:53 +0200

    First commit

---
+++ main.js
@@ -0,0 +1,9 @@
+const electron = require('electron');
+const url = require('url');
+const path = require('path');
+
+const {app, BrowserWindow} = electron;
+
+let mainWindow;
+
+// Listen for app to be ready
