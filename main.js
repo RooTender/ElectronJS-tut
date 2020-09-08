d89a06f4e44e0485a19d972108515c2c6e828b8c
Hubert <lewandowski.hubert486@gmail.com>
2020-09-08 11:18:55 +0200

    First test

--- main.js
+++ main.js
@@ -7,3 +7,14 @@ const {app, BrowserWindow} = electron;
 let mainWindow;
 
 // Listen for app to be ready
+app.on('ready', function() {
+
+    mainWindow = new BrowserWindow({});
+    mainWindow.loadURL(url.format({ // file:./mainWindow.html
+        pathname: path.join(__dirname, 'mainWindow.html'),
+        protocol: 'file:',
+        slashes: true
+    }));
+    
+
+})
